8f1427fd74c4bda7ff041e40d974bdc06f139384
steve.wamai <wamaisw@gmail.com>
2019-11-09 20:59:38 +0300

    added jquery script

---
+++ Js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  $("table").hide();
+  $(".additional-buttons").hide();
+  $(".additional-info").hide();
+  $(".btn.yes").hide();
+  $(".btn.no").hide();
+  $(".additional-info h4").hide();
+
+
+
+
+
+
+
+
+
+})
